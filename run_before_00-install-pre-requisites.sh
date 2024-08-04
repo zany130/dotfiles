b6dc082b16cc6e6c66e53a8a39d14b99c206f32d
zany130 <andresdortiz@gmail.com>
2024-08-03 20:23:16 -0400

    more scripts updates

---
+++ run_before_00-install-pre-requisites.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Install homebrew and bundle if it's not already installed
+if ! command -v brew >/dev/null; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	brew tap Homebrew/bundle
