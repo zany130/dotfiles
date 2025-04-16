60e81fb3435c172a556c5e8576cb6e42054fbff0
Andres <andresdortiz@gmail.com>
2025-04-15 23:41:46 -0400

    Update

--- run_once_after_setup-adb.sh
+++ run_once_after_setup-adb.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# Install ADB if it's not already installed
-if ! command -v adb >/dev/null; then
+# Install ADB udev rules
 	cd ~/Documents || return
-	wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip 
-	unzip -d ~/.local/bin platform-tools-latest-linux.zip 
 	git clone https://github.com/M0Rf30/android-udev-rules.git                                                             ─╯
     cd android-udev-rules || return
     sudo cp -v 51-android.rules /etc/udev/rules.d/51-android.rules
@@ -12,4 +9,3 @@ if ! command -v adb >/dev/null; then
     sudo usermod -a -G adbusers "$(whoami)"
     sudo systemctl restart systemd-udevd.service
     adb kill-server
-fi
--- run_once_before_01-install-pre-requisites.sh
+++ run_once_before_01-install-pre-requisites.sh
@@ -9,3 +9,8 @@ fi
 if ! command -v cargo >/dev/null; then
 	/bin/bash -c "$(curl https://sh.rustup.rs -sSf)"
 fi
+
+# Install AppMan if it's not already installed
+if ! command -v appman >/dev/null; then
+	/bin/bash -c "$(wget -q https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER && chmod a+x ./AM-INSTALLER && ./AM-INSTALLER)"
+fi
