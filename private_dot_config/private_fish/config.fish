375da45272157a09f879ecef3c66ca3f75139f32
Andres <andresdortiz@gmail.com>
2025-09-30 13:52:46 -0400

    Update distrobox.ini Update .var/app/com.github.dynobo.normcap/config/normcap/settings.conf Update .var/app/com.github.hluk.copyq/config/copyq/copyq_geometry.ini Update .var/app/com.github.hluk.copyq/config/copyq/copyq_tabs.ini Update homebrew/settings/Emuchievements/settings.json Update homebrew/settings/decky-autoflatpaks/settings.json Update homebrew/settings/decky-steamgriddb/steamgriddb.json Update homebrew/settings/loader.json Update .config/OpenRGB/plugins/settings/SchedulerSettings.json Update .config/deckboard/config.json Update .config/environment.d/00-path.conf Update .config/fish/config.fish Update .config/rclone/rclone.conf Update .config/systemd/user/gamescope-session-plus@.service.d/override.conf Update .config/warp-terminal/user_preferences.json Update .viminfo

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,3 +3,8 @@ if status is-interactive
     atuin init fish | source
     motivate
 end
+
+set HOMEBREW_COMMAND_NOT_FOUND_HANDLER (brew --repository)/Library/Homebrew/command-not-found/handler.fish
+if test -f $HOMEBREW_COMMAND_NOT_FOUND_HANDLER
+  source $HOMEBREW_COMMAND_NOT_FOUND_HANDLER
+end
